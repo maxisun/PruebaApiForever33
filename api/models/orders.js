9b4f6ac58986ecef5b407e7890cbe2bb823ac69c
maxisun <00354615@uca.edu.sv>
2018-06-25 22:14:00 -0600

    populate y mas verificaciones de salida de parte de anidadciones y productos inexistentes en ordenes

--- api/models/orders.js
+++ api/models/orders.js
@@ -2,11 +2,11 @@
 const mongoose = require('mongoose');
 
 //creacion del Schema de productos
-const ordersSchema = mongoose.Schema({
+const orderSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId, //es un tipo de serial que es como una String larga
-  name: {type: String, required: true}, //nombre es una string
-  price: {type: Number, required: true} //precio es un numero
+  productId: {type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true},//creando una relacion al schema de productos
+  quantity: {type: Number, default: 1}
 });
 
 //esto es para exportar el Schema como un modelo. Como un modelo de objeto para ser construido
-module.exports = mongoose.model('Product', ordersSchema);
+module.exports = mongoose.model('Order', orderSchema);
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,39 +1,109 @@
 const express = require('express');
 const router = express.Router();
+//paquete mongoose
+const mongoose = require('mongoose');
+//importando el modelo de orders
+const Order = require('../models/orders');
+//importando el modelo de productos
+const Product = require('../models/product');
 
 //peticiones GET
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Orders were fetched'
+  Order.find()
+  .select('_id productId quantity')
+  // muestra un merge de los productos linkeados a las ordenes y los campos que queremos que se vean nada mas
+  .populate('productId', '_id name price')
+  .exec()
+  .then(docs => {
+    if (docs.length > 0){
+      res.status(200).json(docs);//retornamos el response de arriba
+    } else { //por si no hay productos
+      res.status(404).json({
+        message: "No orders found"
+      });
+    }
+  })
+  .catch(err => {
+    res.status(500).json({
+      error: err
+    });
   });
 });
 
 //peticiones POST (status 201)
 router.post('/', (req, res, next) => {
-  //creando una orden a partir del req del POST
-  const order = {
-    productId: req.body.productId,
-    quantity: req.body.quantity
-  }
-  res.status(201).json({
-    message: 'Order was created',
-    order: order
-  });
+  //verificando la existencia del id de un producto
+  Product.findById(req.body.productId)
+    .then(product => {
+      //verificando que no sea un producto nulo
+      if (!product) {
+        return res.status(404).json({ //retorna esto y no ejecuta nada mas
+          message: 'Product is inexistent'
+        });
+      }
+      //creando una orden a partir del req del POST
+      const order = new Order({
+        _id: new mongoose.Types.ObjectId(),
+        productId: req.body.productId,
+        quantity: req.body.quantity
+      });
+       return order.save();
+     })
+     .then(result => {
+       console.log(result);
+       res.status(201).json({
+         message: 'Your order has been saved',
+         YourCreatedOrder: {
+           _id: result._id,
+           product: result.productId,
+           quantity: result.quantity
+         }
+       });
+     })
+     .catch(err =>{
+       res.status(500).json({
+         error: err
+       });
+     });
 });
 
 //obteniendo una orden especifica
 router.get('/:orderId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Order details',
-    orderId: req.params.orderId
+  //extrayendo el Id del request
+  const id = req.params.orderId
+  Order.findById(id)
+  .select('_id productId quantity')
+  .populate('productId', '_id name price')
+  .exec()
+  .then(doc => {
+    console.log("From database", doc);
+    //si se encuentra el id y si no se encuentra
+    if (doc) {
+      res.status(200).json(doc);
+    } else {
+      res.status(404).json({message: 'No order found with the provided ID'});
+    }
+  })
+  .catch(err => {
+    res.status(500).json({error:err});
   });
 });
 
 //peticion delete a una orden especifica
 router.delete('/:orderId', (req, res, next) => {
-  res.status(200).json({
-    message: 'Order deleted',
-    orderId: req.params.orderId
+  const id = req.params.orderId
+  Order.findByIdAndRemove(id)
+  .exec()
+  .then(result => {
+    res.status(200).json({
+      message: 'The order was successfully deleted'
+    });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({
+      error: err
+    });
   });
 });
 
